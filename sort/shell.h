a79f8bf0be8d62b0c273cfb6694161a8511f2b72
dainq45972 <1336047549@qq.com>
2024-11-05 17:28:21 +0800

    希尔排序

---
+++ sort/shell.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <algorithm>
+
+template <class Iter,class CMP>
+void shellSort(Iter begin,Iter end,CMP cmp)
+{
+	int len = std::distance(begin,end);
+
+}
--- sort/test.cc
+++ sort/test.cc
@@ -21,13 +21,24 @@ void selectTest()
 void insertTest()
 {
     std::array<int,10> arr{19,88,72,6,556,4,3,25666,12,0};
+	std::array<int,10> arr1{19,88,72,6,556,4,3,25666,12,0};
 
 	insertSort(arr.begin(),arr.end(),[](auto l,auto r){
 	  return *l > *r;
 	});
 
+	insertSort(arr1,[](auto& l,auto& r){
+	  return l > r;
+	});
+
 
 	for(auto i: arr)
+	{
+		std::cout<<i<<std::endl;
+	}
+
+	std::cout<<" ------- "<<std::endl;
+	for(auto i: arr1)
 	{
 		std::cout<<i<<std::endl;
 	}
@@ -61,8 +72,8 @@ void bubbleTest()
 int main()
 {
 	//selectTest();
-	//insertTest();
-    bubbleTest();
+	insertTest();
+    //bubbleTest();
 
 	return 0;
 }
