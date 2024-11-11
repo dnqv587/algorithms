afea86176eed23630318fc4da11b876a80d8c5e0
dainq45972 <1336047549@qq.com>
2024-11-11 09:40:35 +0800

    shell

--- sort/test.cc
+++ sort/test.cc
@@ -1,6 +1,7 @@
 #include "select.h"
 #include "insert.h"
 #include "bubble.h"
+#include "shell.h"
 #include <iostream>
 
 void selectTest()
@@ -69,11 +70,25 @@ void bubbleTest()
     }
 }
 
+void shellTest()
+{
+	std::array<int,10> arr{19,88,72,6,556,4,3,25666,12,0};
+	shellSort(arr.begin(),arr.end(),[](auto l,auto r){
+	  return *l > *r;
+	});
+
+	for(auto i: arr)
+	{
+		std::cout<<i<<std::endl;
+	}
+}
+
 int main()
 {
 	//selectTest();
-	insertTest();
+	//insertTest();
     //bubbleTest();
+	shellTest();
 
 	return 0;
 }
