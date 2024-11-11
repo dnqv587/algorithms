7789fe880a9b297824c67e284192d673f324752f
dainq45972 <1336047549@qq.com>
2024-11-11 09:40:26 +0800

    希尔排序

--- sort/shell.h
+++ sort/shell.h
@@ -5,5 +5,19 @@ template <class Iter,class CMP>
 void shellSort(Iter begin,Iter end,CMP cmp)
 {
 	int len = std::distance(begin,end);
-
+	for(int step=len/3;step>0;step /=3)
+	{
+		for(int i=step;i<len;++i)
+		{
+			auto temp = *(begin+i);
+			for(int j=i;j>step-1;j-=step)
+			{
+				if(cmp(begin+j,&temp))
+				{
+					*(begin+j+step) = *(begin+j);
+					*(begin+j) = temp;
+				}
+			}
+		}
+	}
 }
